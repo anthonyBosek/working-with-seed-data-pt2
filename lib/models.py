0f801b594160faf4c049af7fd2c81cded2058097
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-10 18:59:53 -0700

    complete lab

--- lib/models.py
+++ lib/models.py
@@ -6,37 +6,38 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+
 class Game(Base):
-    __tablename__ = 'games'
+    __tablename__ = "games"
 
     id = Column(Integer(), primary_key=True)
 
-    reviews = relationship('Review', backref='game')
-    users = association_proxy('reviews', 'user',
-        creator=lambda us: Review(user=us))
+    reviews = relationship("Review", backref="game")
+    users = association_proxy("reviews", "user", creator=lambda us: Review(user=us))
 
     def __repr__(self):
-        return f'Game(id={self.id})'
+        return f"Game(id={self.id})"
+
 
 class Review(Base):
-    __tablename__ = 'reviews'
+    __tablename__ = "reviews"
 
     id = Column(Integer(), primary_key=True)
 
-    game_id = Column(Integer(), ForeignKey('games.id'))
-    user_id = Column(Integer(), ForeignKey('users.id'))
+    game_id = Column(Integer(), ForeignKey("games.id"))
+    user_id = Column(Integer(), ForeignKey("users.id"))
 
     def __repr__(self):
-       return f'Review(id={self.id})'
+        return f"Review(id={self.id})"
+
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer(), primary_key=True)
 
-    reviews = relationship('Review', backref='user')
-    games = association_proxy('reviews', 'game',
-        creator=lambda gm: Review(game=gm))
+    reviews = relationship("Review", backref="user")
+    games = association_proxy("reviews", "game", creator=lambda gm: Review(game=gm))
 
     def __repr__(self):
-       return f'User(id={self.id})'
+        return f"User(id={self.id})"
